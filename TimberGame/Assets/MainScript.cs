41425a5f4696196810f5d30e104acaf4f96f6dff
Filipe Moreira <filimor@posteo.com.br>
2020-02-11 23:46:56 -0300

    First assets

---
+++ TimberGame/Assets/MainScript.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+namespace TimberGame
+{
+    public class MainScript : MonoBehaviour
+    {
+        public GameObject FelpudoPlayer;
+        public GameObject FelpudoIdle;
+        public GameObject FelpudoHit;
+
+        private float _horizontalScale;
+
+        private void Start()
+        {
+            _horizontalScale = transform.localScale.x;
+            FelpudoHit.SetActive(false);
+
+        }
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if (Input.mousePosition.x > Screen.width / 2)
+                {
+                    HitRight();
+                }
+                else
+                {
+                    HitLeft();
+                }
+            }
+        }
+
+        private void HitRight()
+        {
+            FelpudoIdle.SetActive(false);
+            FelpudoHit.SetActive(true);
+            FelpudoPlayer.transform.position = new Vector2(0.7f,
+                FelpudoPlayer.transform.position.y);
+            FelpudoPlayer.transform.localScale = new Vector2(-_horizontalScale,
+                FelpudoPlayer.transform.localScale.y);
+            Invoke("SetIdle", 0.25f);
+        }
+
+        private void HitLeft()
+        {
+            FelpudoIdle.SetActive(false);
+            FelpudoHit.SetActive(true);
+            FelpudoPlayer.transform.position = new Vector2(-0.7f,
+                FelpudoPlayer.transform.position.y);
+            FelpudoPlayer.transform.localScale = new Vector2(_horizontalScale,
+                FelpudoPlayer.transform.localScale.y);
+            Invoke("SetIdle", 0.25f);
+        }
+
+        private void SetIdle()
+        {
+            FelpudoIdle.SetActive(true);
+            FelpudoHit.SetActive(false);
+        }
+    }
+}
